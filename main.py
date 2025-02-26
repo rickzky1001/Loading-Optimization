e8d2f00a1a27a3c9c12996e9ed21df5fbc1ac5e6
Rick Zhai <rickzky1001@shu.edu.cn>
2025-02-26 22:45:38 +0800

    add readme

--- main.py
+++ main.py
@@ -13,12 +13,9 @@ if __name__=='__main__':
             PUNISHMENT_VN=1e-2  #车载SKU种类尽量不超过限制
             PUNISHMENT_ND=50/31746   #针对destinaion 3, 尽量满足订单需求
 
-            with open(rf'result/lr_solution_D{destination}_f.pkl', 'rb') as file:  # 注意使用二进制模式读取
-                lr_solution_raw = pickle.load(file) 
-            lr_solution=lr_solution_process(lr_solution_raw,destination=destination)
-            solve(time=3000,lr_solution=lr_solution,destination=destination,c2=c2,c3=c3,PUNISHMENT_VD=PUNISHMENT_VD,PUNISHMENT_VN=PUNISHMENT_VN,PUNISHMENT_ND=PUNISHMENT_ND)
+            solve(time=3000,lr_solution=None,destination=destination,c2=c2,c3=c3,PUNISHMENT_VD=PUNISHMENT_VD,PUNISHMENT_VN=PUNISHMENT_VN,PUNISHMENT_ND=PUNISHMENT_ND)
         else:
-            solve(time=3600*24,lr_solution=None,destination=destination,c2=30,c3=5,PUNISHMENT_VD=1e-3,PUNISHMENT_VN=1e-2)
+            solve(time=3600,lr_solution=None,destination=destination,c2=30,c3=5,PUNISHMENT_VD=1e-3,PUNISHMENT_VN=1e-2)
     
 #对于destination3: ND>c3>VN=VD>c2
 #对于destination124: c2>c3>VD=VN
---
+++ readme.md
@@ -0,0 +1,14 @@
+# 装车优化问题
+## 题目来自上海大学悉尼工商学院2024-2025学年冬季学期现代优化算法课程
+任何问题请与我联系rickzky1001@shu.edu.cn
+## 文件描述
+### solve.py:
+原问题求解函数
+### Linear_Relaxation.py:
+松弛问题求解函数（仅松弛LA变量）
+### utils/vehicles_ublb_calculation.py:
+车辆上界计算函数
+### utils/visualize_log.py:
+可视化松弛解与原问题解求解过程
+### utils/lr_solution_process.py, set_start_values.py:
+尝试将松弛解手动处理以带入原问题。已弃用
